08b5ef309318de921aae1e165522567277db37f2
astroboi <terry007x@korea.ac.kr>
2020-02-21 18:19:28 +0900

    done

--- Main.py
+++ Main.py
@@ -14,17 +14,6 @@ def Main():
         util.make_excel()
 
 
-# class clsMain():
-#
-#     def Main():
-#         srtByBcdGd = clsSortByBarCdGuide()
-#         # get INDEX and Barcode data as dictionary
-#         barcode_dict = srtByBcdGd.getIndexBarcode()
-#         # get sorted data from FASTQ
-#         for tmp_dict in srtByBcdGd.checkFASTQ(barcode_dict):
-#             util = clsUtil(tmp_dict)
-#             util.make_excel()
-
 start_time = clock()
 print("start>>>>>>>>>>>>>>>>>>")
 Main()
